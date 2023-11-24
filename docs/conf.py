0dbc02bfe4508c021b2c782afe968f4bad9538a4
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-11-24 16:34:34 +0100

    Force trigger job

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'datacube-ows'
-copyright = u"2020, Geoscience Australia"
+copyright = u"2023, Geoscience Australia"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
